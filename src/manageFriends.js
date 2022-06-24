497b4d48e22c5af55a76c0db7a2cc520cbc1f71a
Jessica <jess.newman19@gmail.com>
2022-06-24 09:42:13 -0400

    commit tree f075ce897275f3af95b00611ddf99d935f922b86

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,20 @@
+const state = {friends: []}
+const action = { 
+  type: 'friends/add',
+  payload: { 
+    name: "Chrome Boi",
+    hometown: "NYC",
+    id: 1
+  }
+}
+
 export function manageFriends(state, action) {
-  // your code here
+  switch(action.type) { 
+    case 'friends/add': 
+      return {friends: [...state.friends, action.payload]}
+    case 'friends/remove':
+      return {friends: state.friends.filter(friend => friend.id !== action.payload)}
+      default: 
+    return state
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,13 @@
 const initialState = { numberOfPresents: 0 };
+const action = { 
+  type: "presents/increase",
+}
 
-export function managePresents(state = initialState, action) {
-  // your code here
+export function managePresents(state = initialState, action){
+  switch(action.type) { 
+    case 'presents/increase': 
+    return {numberOfPresents: initialState.numberOfPresents + 1}
+    default: 
+      return initialState
+  }
 }
